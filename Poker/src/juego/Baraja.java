285075340e20834b5e8e273857837c442dcd2b52
David <DavidRodriguezPeso@gmail.com>
2021-03-26 15:30:34 +0100

    Versión 0.1

--- Poker/src/juego/Baraja.java
+++ Poker/src/juego/Baraja.java
@@ -3,13 +3,11 @@ package juego;
 import java.util.ArrayList;
 
 public class Baraja {
-	
 	protected static ArrayList<Cartas> baraja = new ArrayList<Cartas>();
 	protected static String[] numeros = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-	protected static char[] palos = {'P', 'T', 'C', 'D'};
+	protected static char[] palos = {'P', 'T', 'C', 'D'}; // P -> Picas, T -> Tréboles, C -> Corazones, D -> Diamantes
 	
 	public static void crearBaraja() {
-		
 		for (int i = 0; i < palos.length; ++i) {
 			for (int j = 0; j < numeros.length; ++j) {
 				baraja.add(new Cartas(numeros[j], palos[i]));
